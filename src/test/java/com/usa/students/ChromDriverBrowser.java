9129934374f9f5322a3ffdc7730326d49fbcde4c
jonyw <jonyw@192.168.0.5>
2018-03-13 18:04:16 -0400

    My first commit

---
+++ src/test/java/com/usa/students/ChromDriverBrowser.java
@@ -0,0 +1,17 @@
+package com.usa.students;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class ChromDriverBrowser {
+	
+        public static void main(String[] args) {
+		System. setProperty("webdriver.chrome.driver", "./DriverBrowser/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize(); 
+
+	}
+
+}
---
+++ src/test/java/com/usa/students/GeckoDriverClass.java
@@ -0,0 +1,12 @@
+package com.usa.students;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+    public class GeckoDriverClass {
+
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.gecko.driver", "./DriverBrowser/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();}}
